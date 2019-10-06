b534ab91553941541bd8d7e33376c704f4d31750
quinnhenschel <qhenschel@gmail.com>
2019-10-06 14:17:58 -0400

    added game state controller script and menus for start and end screen scenes as well as pause

---
+++ Two Space/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float StartTime = 10f;
+    float CurrentTime = 0f;
+
+    public Text CountdownTimerText;
+
+    void Start()
+    {
+        CurrentTime = StartTime;
+    }
+
+    void Update()
+    {
+        CurrentTime -= 1 * Time.deltaTime;
+        CountdownTimerText.text = CurrentTime.ToString("0");
+
+        if(CurrentTime <= 0)
+        {
+            CurrentTime = 0;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}

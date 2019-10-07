23d8581a274c84f063663a79fe3978925e246822
Heather Hennessey <heatherhennessey42@gmail.com>
2019-10-06 21:12:43 -0400

    Gem and Enemy Prefabs with collision functionalities

---
+++ Two Space/Assets/Scripts/Death_Collision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death_Collision : MonoBehaviour {
+
+    
+	void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        Debug.Log("Object entered");
+        GameObject collider = otherObj.gameObject;
+        
+        if(collider.tag == "Player")
+        {
+            Debug.Log("Knight entered");
+            // Ded
+            Player playerScript = collider.transform.parent.GetComponent<Player>();
+            playerScript.alive = false;
+
+        }
+    }
+
+}
+

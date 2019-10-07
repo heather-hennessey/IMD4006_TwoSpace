58143360f7c203c16e967146d9e89260fd9d5101
Quinn Henschel <QuinnHenschel@cunet.carleton.ca>
2019-10-07 12:48:40 -0400

    Tractor beam working

--- Two Space/Assets/Scripts/Death_Collision.cs
+++ Two Space/Assets/Scripts/Death_Collision.cs
@@ -7,12 +7,12 @@ public class Death_Collision : MonoBehaviour {
     
 	void OnTriggerEnter2D(Collider2D otherObj)
     {
-        Debug.Log("Object entered");
+        
         GameObject collider = otherObj.gameObject;
         
         if(collider.tag == "Player")
         {
-            Debug.Log("Knight entered");
+            
             // Ded
             Player pScript = collider.transform.parent.GetComponent<Player>();
             pScript.alive = false;
---
+++ Two Space/Assets/Scripts/beam_collision.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class beam_collision : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D otherObj)
+    {
+        GameObject collider = otherObj.gameObject;
+        //Debug.Log("Hit Object" + collider.tag);
+        if (collider.tag == "Player")
+        {
+           // Debug.Log("Player Enter");
+            // Move up 
+            Player pScript = collider.transform.parent.GetComponent<Player>();
+            pScript.IsInsideTractorBeam = true;
+        }
+        else if(collider.tag == "Item")
+        {
+            Debug.Log("Score up");
+            FindObjectOfType<CountdownTimer>().incrementScore();
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D otherObj)
+    {
+        GameObject collider = otherObj.gameObject;
+        if (collider.tag == "Player")
+        {
+            //Debug.Log("Player Exit");
+            // Move up 
+            Player pScript = collider.transform.parent.GetComponent<Player>();
+            pScript.IsInsideTractorBeam = false;
+        }
+    }
+}
